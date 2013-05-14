f3907ce8d6a285a5e0ae50f1255fd6a37321ef3b
James Pirruccello <james@carbocation.com>
2013-05-14 03:20:22 -0400

    Added test, moved tree into Entry.

--- entry.go
+++ entry.go
@@ -31,6 +31,9 @@ type Entry struct {
 	Seconds      float64 //Seconds since creation
 	Upvotes      int64
 	Downvotes    int64
+	
+	Tree //An Entry has Tree-ness
+	parent, child, sibling *Entry //Mandatory pointer-holders for Tree-ness
 }
 
 // Stores an entry to the database and correctly builds its ancestry based
@@ -76,6 +79,63 @@ func (e *Entry) Persist(parentId int64) error {
 
 func (e Entry) Points() int64 {
 	return e.Upvotes - e.Downvotes
+}
+
+func (e *Entry) Child() *Entry { return e.child }
+func (e *Entry) Sibling() *Entry { return e.sibling }
+func (e *Entry) Parent() *Entry { return e.parent }
+
+func (e *Entry) AddChild(newE *Entry) {
+	if e.child == nil {
+		//Slot is available, directly add the child
+		e.child, newE.parent = newE, e
+	} else {
+		//Slot is unavailable, figure out where the child belongs among peer siblings
+		e.child.addSibling(newE)
+	}
+	
+	return
+}
+
+func (e *Entry) addSibling(newE *Entry) {
+	if newE == nil {
+		return
+	}
+
+	if newE.Points() <= e.Points() {
+		// The new element belongs BELOW the old one
+		if e.sibling == nil {
+			// The old element has no sibling so insertion below it is trivial
+			newE.parent, e.sibling = e, newE
+			
+			return
+		} else {
+			// The old element already has a sibling
+			// Try to add the new element as a sibling of the sibling
+			e.sibling.addSibling(newE)
+			
+			return 
+		}
+	} else {
+		// The new element belongs ABOVE the old one
+		
+		// New element may or may not have a sibling, but we will pop it off and then add it back 
+		// at the end to cover our bases in case it does
+		newESib := newE.sibling
+		
+		if e == e.parent.child {
+			// Old element was a child of its parent
+			e.parent.child, newE.parent, newE.sibling, e.parent = newE, e.parent, e, newE
+		} else {
+			// Old element was presumptively a sibling of its parent
+			e.parent.sibling, newE.parent, newE.sibling, e.parent = newE, e.parent, e, newE
+		}
+		
+		// Add back sibling of new element (if any)
+		newE.addSibling(newESib)
+		
+		return
+	}
 }
 
 //Retrieve one entry by its ID, if it exists. Error if not.
