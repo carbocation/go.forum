0a4215b1b2dfa66ef87c17828c5c9007dfa0ea29
James Pirruccello <james@carbocation.com>
2013-05-14 01:29:03 -0400

    Removed most commented-out code

--- tree.go
+++ tree.go
@@ -48,14 +48,11 @@ func (e *Tree) Sibling() *Tree { return e.sibling }
 func (e *Tree) Parent() *Tree { return e.parent }
 
 func (e *Tree) AddChild(newE *Tree) {
-	//fmt.Printf("Adding %p %v as a child of %p %v\n", newE, newE.Value.Title, e, e.Value.Title)
-	
 	if e.child == nil {
-		//Slot is available
+		//Slot is available, directly add the child
 		e.child, newE.parent = newE, e
 	} else {
-		//Slot is unavailable.
-		//fmt.Printf("%p %v's child slot is full. Adding %p %v as a sibling of %p %v\n\n", e, e.Value.Title, newE, newE.Value.Title, e.child, e.child.Value.Title)
+		//Slot is unavailable, figure out where the child belongs among peer siblings
 		e.child.addSibling(newE)
 	}
 	
@@ -69,7 +66,6 @@ func (e *Tree) addSibling(newE *Tree) {
 
 	if newE.Value.Points() <= e.Value.Points() {
 		// The new element belongs BELOW the old one
-		//fmt.Printf("%p %v should be below %p %v\n\n", newE, newE.Value.Title, e, e.Value.Title)
 		if e.sibling == nil {
 			// The old element has no sibling so insertion below it is trivial
 			newE.parent, e.sibling = e, newE
@@ -84,32 +80,20 @@ func (e *Tree) addSibling(newE *Tree) {
 		}
 	} else {
 		// The new element belongs ABOVE the old one
-		//fmt.Printf("%p %v should be above %p %v\n\n", newE, newE.Value.Title, e, e.Value.Title)
 		
 		// New element may or may not have a sibling, but we will pop it off and then add it back 
 		// at the end to cover our bases in case it does
 		newESib := newE.sibling
 		
-		/*
-		fmt.Printf("e: %p %v\n", e, e)
-		fmt.Printf("newE: %p %v\n", newE, newE)
-		fmt.Printf("e.Parent: %p %v\n", e.parent, e.parent)
-		fmt.Printf("newE.sibling %p %v\n", newE.sibling, newE.sibling)
-		fmt.Printf("newE.parent %p %v\n", newE.parent, newE.parent)
-		*/
-		
 		if e == e.parent.child {
 			// Old element was a child of its parent
-			//fmt.Printf("e (%p) was a child of its parent (%p). Now newE (%p) %v will interpose.\n", e, e.parent, newE, newE.Value.Title)
-			
 			e.parent.child, newE.parent, newE.sibling, e.parent = newE, e.parent, e, newE
 		} else {
 			// Old element was presumptively a sibling of its parent
-			//fmt.Printf("e (%p) %v was a sib of its parent (%p) %v. Now newE (%p) %v will interpose.\n", e, e.Value.Title, e.parent, e.parent.Value.Title, newE, newE.Value.Title)
-			
 			e.parent.sibling, newE.parent, newE.sibling, e.parent = newE, e.parent, e, newE
 		}
 		
+		// Add back sibling of new element (if any)
 		newE.addSibling(newESib)
 		
 		return
@@ -131,9 +115,8 @@ func (e *Tree) Walk() {
 
 func main () {
 	t := New(Entry{Title: "Root", Upvotes: 10})
+	t.AddChild(New(Entry{Title: "Depth 1 #4", Upvotes: 0}))
 	t.AddChild(New(Entry{Title: "Depth 1 #3", Upvotes: 1}))
-	//t.AddChild(New(Entry{Title: "Level 2 #2", Upvotes: 0}))
-	//t.AddChild(New(Entry{Title: "Level 2 #3", Upvotes: 1}))
 	
 	
 	t2 := New(Entry{Title: "Depth 1 #2", Upvotes: 2})
